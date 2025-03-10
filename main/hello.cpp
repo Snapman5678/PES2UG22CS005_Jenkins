6a41e1f6d3fd5ecc66daaa88dd1a37152867a5c3
Snapman5678 <aamirmohammed5678@gmail.com>
2025-03-10 16:16:10 +0530

    hello.cpp ver something

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Bow before me peasants as i have an internship(jk no offense)\n";
-  cout << "<Changed version> I apologise for my previous comments \n";
+  cout << "<Changed version> I apologise  \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
