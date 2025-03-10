34b704407910629dd3ead1c1ca2ec3a09bb024ae
Snapman5678 <aamirmohammed5678@gmail.com>
2025-03-10 19:28:03 +0530

    rando message

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Bow before me peasants as i have an internship(jk no offense)\n";
-  cout << "<Changed version> I do not apologise  \n";
+  cout << "SRN is PES2UG22CS005 \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
