dd1a4ee1542716afb9b96d1d5995a61766648eef
Snapman5678 <aamirmohammed5678@gmail.com>
2025-03-10 19:57:34 +0530

    comomit num 3

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Bow before me peasants as i have an internship(jk no offense)\n";
-  cout << "SRN is PES2UG22CS005 and this should work \n";
+  cout << "SRN is PES2UG22CS005 and this should work - num 3\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
